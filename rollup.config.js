96a285f435d3f94c9c40c4a5794e7842c70a076a
lleragon <96912020+lleragon@users.noreply.github.com>
2026-03-14 21:45:59 +0100

    chore: removed rollup serve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import serve from 'rollup-plugin-serve';
 import json from '@rollup/plugin-json';
 import terser from '@rollup/plugin-terser';
 import {literalsHtmlCssMinifier} from '@literals/rollup-plugin-html-css-minifier';
 import pkg from './package.json' with {type: 'json'};
 
 const dev = process.env.ROLLUP_WATCH;
-// const serveopts = { todo
-//     contentBase: ['./dist'],
-//     host: '0.0.0.0',
-//     port: 5000,
-//     allowCrossOrigin: true,
-//     headers: {
-//         'Access-Control-Allow-Origin': '*',
-//     },
-// };
 
 // https://github.com/d3/d3-interpolate/issues/58
 const D3_WARNING = /Circular dependency.*d3-interpolate/
@@ -42,7 +32,6 @@ export default [
             commonjs(),
             json(),
             babel({babelHelpers: 'bundled'}),
-            //dev && serve(serveopts), todo
             !dev && terser({
                 ecma: 2015, // ES6
                 compress: {
